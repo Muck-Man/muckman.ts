6db7216b98b675211c8cc5ddb3526689fe3a03ba
Dan <dansoul.cake@gmail.com>
2019-08-13 00:54:44 -0400

    use the onPage function on the paginator instead of premaking the embeds

--- src/commands/activity.ts
+++ src/commands/activity.ts
@@ -37,178 +37,175 @@ export default (<Command.CommandOptions> {
 
     const presence = user.presence;
     const activities = (presence) ? presence.activities.toArray() : [];
-    const embeds: Array<Utils.Embed> = [];
+    const pageLimit = activities.length || 1;
 
-    for (let activityId = 0; activityId < (activities.length || 1); activityId++) {
-      const embed = new Utils.Embed();
-      embed.setAuthor(
-        user.toString(),
-        user.avatarUrlFormat(null, {size: 1024}),
-        user.jumpLink,
-      );
-      embed.setColor(PresenceStatusColors['offline']);
+    const paginator = new Paginator(context, {
+      page: Math.max(1, Math.min(args.id, pageLimit)),
+      pageLimit,
+      onPage: (page) => {
+        const embed = new Utils.Embed();
+        embed.setAuthor(
+          user.toString(),
+          user.avatarUrlFormat(null, {size: 1024}),
+          user.jumpLink,
+        );
+        embed.setColor(PresenceStatusColors['offline']);
 
-      if (presence) {
-        if (presence.clientStatus) {
-          const description = [];
-          for (let key of Object.keys(presence.clientStatus).sort()) {
-            let status = (<any> presence.clientStatus)[key];
+        if (presence) {
+          if (presence.clientStatus) {
+            const description = [];
+            for (let key of Object.keys(presence.clientStatus).sort()) {
+              let status = (<any> presence.clientStatus)[key];
+              if (status in PresenceStatusTexts) {
+                status = PresenceStatusTexts[status];
+              }
+              description.push(`**${toTitleCase(key)}**: ${status}`);
+            }
+            embed.addField('Status', description.join('\n'));
+          } else {
+            let status = presence.status;
             if (status in PresenceStatusTexts) {
               status = PresenceStatusTexts[status];
             }
-            description.push(`**${toTitleCase(key)}**: ${status}`);
+            embed.addField('Status', status, true);
           }
-          embed.addField('Status', description.join('\n'));
-        } else {
-          let status = presence.status;
-          if (status in PresenceStatusTexts) {
-            status = PresenceStatusTexts[status];
+  
+          if (presence.status in PresenceStatusColors) {
+            embed.setColor(PresenceStatusColors[presence.status]);
           }
-          embed.addField('Status', status, true);
-        }
 
-        if (presence.status in PresenceStatusColors) {
-          embed.setColor(PresenceStatusColors[presence.status]);
-        }
-
-        if (activityId in activities) {
-          const activity = activities[activityId];
-          if (activity) {
-            const description = [];
-            if (activity.isCustomStatus) {
-              description.push(`Custom Status: ${activity.state}`);
-
-              if (activity.details) {
-                try {
-                  const details = new URLSearchParams(activity.details);
-                  const channelId = details.get('c') || '';
-                  if (context.channels.has(channelId)) {
-                    const channel = <Structures.Channel> context.channels.get(channelId);
-                    if (channel.isGuildVoice) {
-                      description.push(`In Voice: ${channel.mention} (${channel.id})`);
-                    }
-                  }
-                } catch(error) {
-
-                }
-              }
-            } else {
-              const text = [activity.typeText, activity.name];
-              description.push(text.filter((v) => v).join(' '));
-              if (activity.isOnSpotify) {
-                if (activity.assets && activity.assets.largeText) {
-                  description.push(`**Album**: ${activity.assets.largeText}`);
-                }
+          const activityId = page - 1;
+          if (activityId in activities) {
+            const activity = activities[activityId];
+            if (activity) {
+              const description = [];
+              if (activity.isCustomStatus) {
+                description.push(`Custom Status: ${activity.state}`);
+  
                 if (activity.details) {
-                  description.push(`**Song**: ${activity.details}`);
-                }
-                if (activity.state) {
-                  description.push(`**Artists**: ${activity.state.split('; ').join(', ')}`);
+                  try {
+                    const details = new URLSearchParams(activity.details);
+                    const channelId = details.get('c') || '';
+                    if (context.channels.has(channelId)) {
+                      const channel = <Structures.Channel> context.channels.get(channelId);
+                      if (channel.isGuildVoice) {
+                        description.push(`In Voice: ${channel.mention} (${channel.id})`);
+                      }
+                    }
+                  } catch(error) {
+  
+                  }
                 }
               } else {
-                if (activity.details) {
-                  description.push(`**Details**: ${activity.details}`);
+                const text = [activity.typeText, activity.name];
+                description.push(text.filter((v) => v).join(' '));
+                if (activity.isOnSpotify) {
+                  if (activity.assets && activity.assets.largeText) {
+                    description.push(`**Album**: ${activity.assets.largeText}`);
+                  }
+                  if (activity.details) {
+                    description.push(`**Song**: ${activity.details}`);
+                  }
+                  if (activity.state) {
+                    description.push(`**Artists**: ${activity.state.split('; ').join(', ')}`);
+                  }
+                } else {
+                  if (activity.details) {
+                    description.push(`**Details**: ${activity.details}`);
+                  }
+                  if (activity.state) {
+                    description.push(`**State**: ${activity.state}`);
+                  }
                 }
-                if (activity.state) {
-                  description.push(`**State**: ${activity.state}`);
-                }
-              }
-              if (activity.isOnXbox) {
-                description.push('**On Xbox**');
-              }
-            }
-            let name = 'Activity';
-            if (1 < activities.length) {
-              name = `Activity (${activityId + 1} of ${activities.length})`;
-            }
-            embed.addField(name, description.join('\n'), true);
-          }
-
-          // now do the fields
-          const thumbnail = activity.imageUrlFormat(null, {size: 1024});
-          if (thumbnail) {
-            embed.setThumbnail(thumbnail);
-          }
-
-          const assets = activity.assets;
-          if (assets && assets.smallText) {
-            embed.setFooter(
-              assets.smallText,
-              assets.smallImageUrl || undefined,
-            );
-          }
-
-          const timestamps = activity.timestamps;
-          if (timestamps) {
-            const elapsed = formatTime(Math.max(timestamps.elapsedTime, 0));
-            const total = formatTime(timestamps.totalTime);
-
-            const description = (timestamps.end) ? `${elapsed}/${total}` : `${elapsed}`;
-            embed.addField('Time', description, true);
-          }
-
-          if (activity.party) {
-            const currentSize = activity.party.currentSize;
-            const maxSize = activity.party.maxSize;
-            const group = activity.party.group.map((user) => {
-              return user.mention;
-            });
-            const description = [];
-            if (activity.party.size) {
-              description.push(`${currentSize}/${maxSize} members`);
-            }
-            if (group.length) {
-              description.push(`**Group (${group.length})**: ${group.join(', ')}`);
-            }
-            if (description.length) {
-              embed.addField('Party', description.join('\n'));
-            }
-          }
-
-          const application = activity.application;
-          if (application) {
-            const description = new Set();
-            if (application.isOnDiscord) {
-              description.add(`[**Discord**](${application.jumpLink})`);
-            }
-            if (application.thirdPartySkus) {
-              for (let [key, thirdPartySku] of application.thirdPartySkus) {
-                const url = thirdPartySku.url;
-                if (url) {
-                  description.add(`[**${thirdPartySku.name}**](${url})`);
+                if (activity.isOnXbox) {
+                  description.push('**On Xbox**');
                 }
               }
+              let name = 'Activity';
+              if (1 < pageLimit) {
+                name = `Activity (${page} of ${pageLimit})`;
+              }
+              embed.addField(name, description.join('\n'), true);
             }
-            if (description.size) {
-              embed.addField('Store Links', Array.from(description).join(', '), true);
+  
+            // now do the fields
+            const thumbnail = activity.imageUrlFormat(null, {size: 1024});
+            if (thumbnail) {
+              embed.setThumbnail(thumbnail);
+            }
+  
+            const assets = activity.assets;
+            if (assets && assets.smallText) {
+              embed.setFooter(
+                assets.smallText,
+                assets.smallImageUrl || undefined,
+              );
+            }
+  
+            const timestamps = activity.timestamps;
+            if (timestamps) {
+              const elapsed = formatTime(Math.max(timestamps.elapsedTime, 0));
+              const total = formatTime(timestamps.totalTime);
+  
+              const description = (timestamps.end) ? `${elapsed}/${total}` : `${elapsed}`;
+              embed.addField('Time', description, true);
+            }
+  
+            if (activity.party) {
+              const currentSize = activity.party.currentSize;
+              const maxSize = activity.party.maxSize;
+              const group = activity.party.group.map((user) => {
+                return user.mention;
+              });
+              const description = [];
+              if (activity.party.size) {
+                description.push(`${currentSize}/${maxSize} members`);
+              }
+              if (group.length) {
+                description.push(`**Group (${group.length})**: ${group.join(', ')}`);
+              }
+              if (description.length) {
+                embed.addField('Party', description.join('\n'));
+              }
+            }
+  
+            const application = activity.application;
+            if (application) {
+              const description = new Set();
+              if (application.isOnDiscord) {
+                description.add(`[**Discord**](${application.jumpLink})`);
+              }
+              if (application.thirdPartySkus) {
+                for (let [key, thirdPartySku] of application.thirdPartySkus) {
+                  const url = thirdPartySku.url;
+                  if (url) {
+                    description.add(`[**${thirdPartySku.name}**](${url})`);
+                  }
+                }
+              }
+              if (description.size) {
+                embed.addField('Store Links', Array.from(description).join(', '), true);
+              }
+            }
+  
+            if (activity.isOnSpotify) {
+              const description = [];
+              if (activity.details) {
+                description.push(`[**${activity.details}**](${activity.spotifyTrackUrl})`);
+              } else {
+                description.push(<string> activity.spotifyTrackUrl);
+              }
+              embed.addField('Spotify Track', description.join('\n'), true);
             }
           }
-
-          if (activity.isOnSpotify) {
-            const description = [];
-            if (activity.details) {
-              description.push(`[**${activity.details}**](${activity.spotifyTrackUrl})`);
-            } else {
-              description.push(<string> activity.spotifyTrackUrl);
-            }
-            embed.addField('Spotify Track', description.join('\n'), true);
-          }
+        } else {
+          embed.addField('Activity', PresenceStatusTexts['offline']);
         }
-      } else {
-        embed.addField('Activity', PresenceStatusTexts['offline']);
-      }
 
-      embeds.push(embed);
-    }
-
-    const page = Math.max(1, Math.min(args.id, embeds.length));
-    if (1 < embeds.length && context.message.canReact) {
-      const paginator = new Paginator(context, {page, pages: embeds});
-      await paginator.start();
-    } else {
-      const embed = <any> embeds[page - 1];
-      await context.reply({embed});
-    }
+        return embed;
+      },
+    });
+    await paginator.start();
   },
   onError: (context, args, error) => {
     console.error(error);
--- src/commands/user.ts
+++ src/commands/user.ts
@@ -34,132 +34,128 @@ export default (<Command.CommandOptions> {
 
     const presence = user.presence;
     const activities = (presence) ? presence.activities.toArray() : [];
-    const embeds: Array<Utils.Embed> = [];
+    const pageLimit = activities.length || 1;
 
-    for (let activityId = 0; activityId < (activities.length || 1); activityId++) {
-      const embed = new Utils.Embed();
-      embed.setAuthor(user.toString(), user.avatarUrlFormat(null, {size: 1024}), user.jumpLink);
-      embed.setColor(PresenceStatusColors['offline']);
-      embed.setThumbnail(user.avatarUrlFormat(null, {size: 1024}));
+    const paginator = new Paginator(context, {
+      page: Math.max(1, Math.min(args.id, pageLimit)),
+      pageLimit,
+      onPage: (page) => {
+        const embed = new Utils.Embed();
+        embed.setAuthor(user.toString(), user.avatarUrlFormat(null, {size: 1024}), user.jumpLink);
+        embed.setColor(PresenceStatusColors['offline']);
+        embed.setThumbnail(user.avatarUrlFormat(null, {size: 1024}));
 
-      embed.addField('Information', [
-        `**Id**: \`${user.id}\``,
-        `**Bot**: ${(user.bot) ? 'Yes' : 'No'}`,
-      ].join('\n'), true);
+        embed.addField('Information', [
+          `**Id**: \`${user.id}\``,
+          `**Bot**: ${(user.bot) ? 'Yes' : 'No'}`,
+        ].join('\n'), true);
 
-      const dateOptions = {
-        hour12: false,
-        timeZone: 'America/New_York',
-      };
-      embed.addField('Joined', [
-        `**Discord**: ${user.createdAt.toLocaleString('en-US', dateOptions)}`,
-        (isMember) ? `**Guild**: ${member.joinedAt.toLocaleString('en-US', dateOptions)}` : null,
-      ].filter((v) => v).join('\n'), true);
+        const dateOptions = {
+          hour12: false,
+          timeZone: 'America/New_York',
+        };
+        embed.addField('Joined', [
+          `**Discord**: ${user.createdAt.toLocaleString('en-US', dateOptions)}`,
+          (isMember) ? `**Guild**: ${member.joinedAt.toLocaleString('en-US', dateOptions)}` : null,
+        ].filter((v) => v).join('\n'), true);
 
-      if (isMember) {
-        const roles = member.roles.map((role, roleId) => {
-          return (role) ? `\`${role.name}\`` : `<@${roleId}>`;
-        });
-        const voiceChannel = member.voiceChannel;
-        embed.addField('Guild Specific', [
-          (member.premiumSince) ? `**Boosting Since**: ${member.premiumSince.toLocaleString('en-US', dateOptions)}` : null,
-          (member.nick) ? `**Nickname**: ${member.nick}` : null,
-          (member.isOwner) ? '**Owner**: Yes' : null,
-          `**Roles (${member.roles.length})**: ${roles.join(', ')}`,
-          (voiceChannel) ? `**Voice**: ${voiceChannel.toString()}` : null,
-        ].filter((v) => v).join('\n'));
-      }
-
-      if (presence) {
-        if (presence.status in PresenceStatusColors) {
-          embed.setColor(PresenceStatusColors[presence.status]);
+        if (isMember) {
+          const roles = member.roles.map((role, roleId) => {
+            return (role) ? `\`${role.name}\`` : `<@${roleId}>`;
+          });
+          const voiceChannel = member.voiceChannel;
+          embed.addField('Guild Specific', [
+            (member.premiumSince) ? `**Boosting Since**: ${member.premiumSince.toLocaleString('en-US', dateOptions)}` : null,
+            (member.nick) ? `**Nickname**: ${member.nick}` : null,
+            (member.isOwner) ? '**Owner**: Yes' : null,
+            `**Roles (${member.roles.length})**: ${roles.join(', ')}`,
+            (voiceChannel) ? `**Voice**: ${voiceChannel.toString()}` : null,
+          ].filter((v) => v).join('\n'));
         }
 
-        if (presence.clientStatus) {
-          const description = [];
-          for (let key of Object.keys(presence.clientStatus).sort()) {
-            let status = (<any> presence.clientStatus)[key];
+        if (presence) {
+          if (presence.status in PresenceStatusColors) {
+            embed.setColor(PresenceStatusColors[presence.status]);
+          }
+
+          if (presence.clientStatus) {
+            const description = [];
+            for (let key of Object.keys(presence.clientStatus).sort()) {
+              let status = (<any> presence.clientStatus)[key];
+              if (status in PresenceStatusTexts) {
+                status = PresenceStatusTexts[status];
+              }
+              description.push(`**${toTitleCase(key)}**: ${status}`);
+            }
+            embed.addField('Status', description.join('\n'));
+          } else {
+            let status = presence.status;
             if (status in PresenceStatusTexts) {
               status = PresenceStatusTexts[status];
             }
-            description.push(`**${toTitleCase(key)}**: ${status}`);
+            embed.addField('Status', status, true);
           }
-          embed.addField('Status', description.join('\n'));
-        } else {
-          let status = presence.status;
-          if (status in PresenceStatusTexts) {
-            status = PresenceStatusTexts[status];
-          }
-          embed.addField('Status', status, true);
-        }
 
-        if (activityId in activities) {
-          const activity = activities[activityId];
-          if (activity) {
-            const description = [];
-            if (activity.isCustomStatus) {
-              description.push(`Custom Status: ${activity.state}`);
+          const activityId = page - 1;
+          if (activityId in activities) {
+            const activity = activities[activityId];
+            if (activity) {
+              const description = [];
+              if (activity.isCustomStatus) {
+                description.push(`Custom Status: ${activity.state}`);
 
-              if (activity.details) {
-                try {
-                  const details = new URLSearchParams(activity.details);
-                  const channelId = details.get('c') || '';
-                  if (context.channels.has(channelId)) {
-                    const channel = <Structures.Channel> context.channels.get(channelId);
-                    if (channel.isGuildVoice) {
-                      description.push(`In Voice: ${channel.mention} (${channel.id})`);
-                    }
-                  }
-                } catch(error) {
-
-                }
-              }
-            } else {
-              const text = [activity.typeText, activity.name];
-              description.push(text.filter((v) => v).join(' '));
-              if (activity.isOnSpotify) {
-                if (activity.assets && activity.assets.largeText) {
-                  description.push(`**Album**: ${activity.assets.largeText}`);
-                }
                 if (activity.details) {
-                  description.push(`**Song**: ${activity.details}`);
-                }
-                if (activity.state) {
-                  description.push(`**Artists**: ${activity.state.split('; ').join(', ')}`);
+                  try {
+                    const details = new URLSearchParams(activity.details);
+                    const channelId = details.get('c') || '';
+                    if (context.channels.has(channelId)) {
+                      const channel = <Structures.Channel> context.channels.get(channelId);
+                      if (channel.isGuildVoice) {
+                        description.push(`In Voice: ${channel.mention} (${channel.id})`);
+                      }
+                    }
+                  } catch(error) {
+
+                  }
                 }
               } else {
-                if (activity.details) {
-                  description.push(`**Details**: ${activity.details}`);
+                const text = [activity.typeText, activity.name];
+                description.push(text.filter((v) => v).join(' '));
+                if (activity.isOnSpotify) {
+                  if (activity.assets && activity.assets.largeText) {
+                    description.push(`**Album**: ${activity.assets.largeText}`);
+                  }
+                  if (activity.details) {
+                    description.push(`**Song**: ${activity.details}`);
+                  }
+                  if (activity.state) {
+                    description.push(`**Artists**: ${activity.state.split('; ').join(', ')}`);
+                  }
+                } else {
+                  if (activity.details) {
+                    description.push(`**Details**: ${activity.details}`);
+                  }
+                  if (activity.state) {
+                    description.push(`**State**: ${activity.state}`);
+                  }
                 }
-                if (activity.state) {
-                  description.push(`**State**: ${activity.state}`);
+                if (activity.isOnXbox) {
+                  description.push('**On Xbox**');
                 }
               }
-              if (activity.isOnXbox) {
-                description.push('**On Xbox**');
+              let name = 'Activity';
+              if (1 < pageLimit) {
+                name = `Activity (${page} of ${pageLimit})`;
               }
+              embed.addField(name, description.join('\n'), true);
             }
-            let name = 'Activity';
-            if (1 < activities.length) {
-              name = `Activity (${activityId + 1} of ${activities.length})`;
-            }
-            embed.addField(name, description.join('\n'), true);
           }
+        } else {
+          embed.addField('Activity', PresenceStatusTexts['offline']);
         }
-      } else {
-        embed.addField('Activity', PresenceStatusTexts['offline']);
-      }
-
-      embeds.push(embed);
-    }
-  
-    const page = Math.max(1, Math.min(args.id, embeds.length));
-    if (1 < embeds.length && context.message.canReact) {
-      const paginator = new Paginator(context, {page, pages: embeds});
-      await paginator.start();
-    } else {
-      const embed = <any> embeds[page - 1];
-      await context.reply({embed});
-    }
+        return embed;
+      },
+    });
+    await paginator.start();
   },
 });
